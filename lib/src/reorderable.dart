6a4ec62a3deaf6c491b8ded6a8bb4949b27c5236
Alexander Mudzo <alexander.mudzo@gmail.com>
2024-05-14 17:32:30 +0200

    migrated to flutter Gradle's block plugin syntax, removed unused files

--- lib/src/reorderable.dart
+++ lib/src/reorderable.dart
@@ -6,6 +6,7 @@ import 'src.dart';
 typedef ReorderableBuilder = Widget Function(
   BuildContext context,
   Animation<double> animation,
+  // ignore: avoid_positional_boolean_parameters
   bool inDrag,
 );
 
